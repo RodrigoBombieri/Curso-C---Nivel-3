9015c9449f281a1b31564da62026f12fac40c48d
Rodrigo Bombieri <rodrigosbombieri@gmail.com>
2024-01-02 20:50:25 -0300

    Terminando las funcionalidades de modificar y eliminar

--- 4.Eventos/ejemploDeGrilla/ejemploDeGrilla/AutoForm.aspx.cs
+++ 4.Eventos/ejemploDeGrilla/ejemploDeGrilla/AutoForm.aspx.cs
@@ -19,39 +19,42 @@ namespace ejemploDeGrilla
             ddlColores.Items.Add("Negro");
             ddlColores.Items.Add("Blanco");
 
-            /// Con esto verifico que el ID en la URL sea valido 
-            if (Request.QueryString["id"] != null)
+            if (!IsPostBack)
             {
-                /// Si entró Significa que vengo desde el boton Seleccionar con un ID precargado
-                
-                /// Guardamos el ID en una variable
-                int id = int.Parse(Request.QueryString["id"].ToString());
-                /// Me traigo de la Session, la lista temporal de autos
-                List<Auto> temporal = (List<Auto>)Session["listaAutos"];
-                /// Para buscar el auto seleccionado usando lambra expression
-                /// Me va a devolver de la lista el objeto seleccionado
-                Auto seleccionado = temporal.Find( x => x.Id == id );
-                /// Precarga en las cajas de texto los datos del elemento seleccionado
-                txtModelo.Text = seleccionado.Modelo;
-                
-                txtID.Text = seleccionado.Id.ToString();
-                txtID.ReadOnly = true;
-                txtDescripcion.Text = seleccionado.Descripcion;
+                /// Con esto verifico que el ID en la URL sea valido 
+                if (Request.QueryString["id"] != null)
+                {
+                    /// Si entró Significa que vengo desde el boton Seleccionar con un ID precargado
 
-                txtFecha.Text = seleccionado.Fecha.ToString();
+                    /// Guardamos el ID en una variable
+                    int id = int.Parse(Request.QueryString["id"].ToString());
+                    /// Me traigo de la Session, la lista temporal de autos
+                    List<Auto> temporal = (List<Auto>)Session["listaAutos"];
+                    /// Para buscar el auto seleccionado usando lambda expression
+                    /// Me va a devolver de la lista el objeto seleccionado
+                    Auto seleccionado = temporal.Find(x => x.Id == id);
+                    /// Precarga en las cajas de texto los datos del elemento seleccionado
+                    txtModelo.Text = seleccionado.Modelo;
 
-                ddlColores.SelectedValue = seleccionado.Color;
+                    txtID.Text = seleccionado.Id.ToString();
+                    txtID.ReadOnly = true;
+                    txtDescripcion.Text = seleccionado.Descripcion;
 
-                chkUsado.Checked = seleccionado.Usado;
+                    txtFecha.Text = seleccionado.Fecha.ToString();
 
-                rdbImportado.Checked = seleccionado.Importado;
-                /// Si venimos desde el boton Seleccionar de la grilla, el boton Aceptar se deshabilita
-                btnAceptar.Enabled=false;
-            }
-            else
-            {
-                btnModificar.Enabled = false;
-                btnEliminar.Enabled = false;
+                    ddlColores.SelectedValue = seleccionado.Color;
+
+                    chkUsado.Checked = seleccionado.Usado;
+
+                    rdbImportado.Checked = seleccionado.Importado;
+                    /// Si venimos desde el boton Seleccionar de la grilla, el boton Aceptar se deshabilita
+                    btnAceptar.Enabled = false;
+                }
+                else
+                {
+                    btnModificar.Enabled = false;
+                    btnEliminar.Enabled = false;
+                }
             }
         }
 
@@ -68,16 +71,16 @@ namespace ejemploDeGrilla
             a.Fecha = DateTime.Parse(txtFecha.Text);
             a.Usado = chkUsado.Checked;
             /// Capturo el estado de los radioButton
-            if(rdbImportado.Checked)
+            if (rdbImportado.Checked)
             {
                 a.Importado = true;
             }
-            else if(rdbNacional.Checked)
+            else if (rdbNacional.Checked)
             {
                 a.Importado = false;
             }
             /// -----> Ya tengo el auto cargado <------
-            
+
 
             /// Recuperamos la lista de la Session para poder agregarle un elemento
             /// Con esto transformamos la lista de autos en una lista de autos con un casteo explicito 
@@ -90,12 +93,40 @@ namespace ejemploDeGrilla
 
         protected void btnModificar_Click(object sender, EventArgs e)
         {
-            
+            int id = int.Parse(Request.QueryString["id"].ToString());
+            List<Auto> temporal = (List<Auto>)Session["listaAutos"];
+            Auto seleccionado = temporal.Find(x => x.Id == id);
+
+            if (seleccionado != null)
+            {
+                seleccionado.Modelo = txtModelo.Text;
+                seleccionado.Descripcion = txtDescripcion.Text;
+                seleccionado.Color = ddlColores.SelectedValue;
+                seleccionado.Fecha = DateTime.TryParse(txtFecha.Text, out DateTime parsedDate) ? parsedDate : DateTime.Now;
+                seleccionado.Usado = chkUsado.Checked;
+                if (rdbImportado.Checked)
+                {
+                    seleccionado.Importado = true;
+                }
+                else if (rdbNacional.Checked)
+                {
+                    seleccionado.Importado = false;
+                }
+                Response.Redirect("Default.aspx");
+            }
         }
 
         protected void btnEliminar_Click(object sender, EventArgs e)
         {
+            int id = int.Parse(Request.QueryString["id"].ToString());
+            List<Auto> temporal = (List<Auto>)Session["listaAutos"];
+            Auto seleccionado = temporal.Find(x => x.Id == id);
 
+            if (seleccionado != null)
+            {
+                ((List<Auto>)Session["listaAutos"]).Remove(seleccionado);
+                Response.Redirect("Default.aspx");
+            }
         }
     }
 }
--- 4.Eventos/ejemploDeGrilla/ejemploDeGrilla/FormPractica.aspx.cs
+++ 4.Eventos/ejemploDeGrilla/ejemploDeGrilla/FormPractica.aspx.cs
@@ -12,38 +12,65 @@ namespace ejemploDeGrilla
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (Request.QueryString["id"] != null)
+            if (!IsPostBack)
             {
-                int id = int.Parse(Request.QueryString["id"].ToString());
-                List<Direccion> temporal = (List<Direccion>)Session["listaDireccion"];
-                Direccion seleccionado = temporal.Find(x => x.Id == id);
-                txtID.Text = seleccionado.Id.ToString();
-                txtID.ReadOnly = true;
-                txtCalle.Text = seleccionado.Calle;
-                txtNumero.Text = seleccionado.Numero.ToString();
-
-                btnAceptar.Enabled = false;
-            }
-            else
-            {
-                btnModificar.Enabled = false;
-                btnEliminar.Enabled = false;
+                if (Request.QueryString["id"] != null)
+                {
+                    int id = int.Parse(Request.QueryString["id"].ToString());
+                    List<Direccion> temporal = (List<Direccion>)Session["listaDireccion"];
+                    Direccion seleccionado = temporal.Find(x => x.Id == id);
+                    txtID.Text = seleccionado.Id.ToString();
+                    txtID.ReadOnly = true;
+                    txtCalle.Text = seleccionado.Calle;
+                    txtNumero.Text = seleccionado.Numero.ToString();
+                    btnAceptar.Enabled = false;
+                }
+                else
+                {
+                    btnModificar.Enabled = false;
+                    btnEliminar.Enabled = false;
+                }
             }
         }
 
         protected void btnAceptar_Click(object sender, EventArgs e)
         {
+            Direccion d = new Direccion();
+            d.Id = int.Parse(txtID.Text);
+            d.Calle = txtCalle.Text;
+            d.Numero = int.Parse(txtNumero.Text);
 
+            ((List<Direccion>)Session["listaDireccion"]).Add(d);
+            Response.Redirect("Default.aspx");
         }
 
         protected void btnModificar_Click(object sender, EventArgs e)
         {
+            int id = int.Parse(Request.QueryString["id"].ToString());
+            List<Direccion> temporal = (List<Direccion>)Session["listaDireccion"];
+            Direccion seleccionado = temporal.Find(x => x.Id == id);
 
+            if (seleccionado != null)
+            {
+                seleccionado.Calle = txtCalle.Text;
+                seleccionado.Numero = int.Parse(txtNumero.Text);
+                Response.Redirect("Default.aspx");
+
+            }
+                
         }
 
         protected void btnEliminar_Click(object sender, EventArgs e)
         {
-
+            int id = int.Parse(Request.QueryString["id"].ToString());
+            List<Direccion> temporal = (List<Direccion>)Session["listaDireccion"];
+            Direccion seleccionado = temporal.Find(x => x.Id == id);
+            
+            if(seleccionado != null)
+            {
+                ((List<Direccion>)Session["listaDireccion"]).Remove(seleccionado);
+                Response.Redirect("Default.aspx");
+            }
         }
     }
 }
