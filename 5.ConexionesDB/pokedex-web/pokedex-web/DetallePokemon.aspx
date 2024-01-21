91973e5ba6433874492c454835cee864415bf4c8
Rodrigo Bombieri <rodrigosbombieri@gmail.com>
2024-01-21 12:22:58 -0300

    Mejorando estilos de Detalle Pokemons

--- 5.ConexionesDB/pokedex-web/pokedex-web/DetallePokemon.aspx
+++ 5.ConexionesDB/pokedex-web/pokedex-web/DetallePokemon.aspx
@@ -1,14 +1,58 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="DetallePokemon.aspx.cs" Inherits="pokedex_web.DetallePokemon" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style>
+        .custom-card {
+            width: 500px;
+            height: 500px;
+            transition: transform 0.2s;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-radius: 8px;
+            overflow: hidden;
+        }
+
+            .custom-card:hover {
+                transform: scale(1.05);
+                box-shadow: 0 0 10px rgb(245, 183, 177);
+            }
+
+        .card-title,
+        .card-text {
+            margin-bottom: 8px;
+            font-size: 20px;
+        }
+
+        .btn-favorite {
+            text-decoration: none;
+            font-size: 40px;
+            color: darkcyan;
+        }
+
+            .btn-favorite:hover {
+                color: gold;
+            }
+
+        .imagenPoke {
+            width: 400px;
+            height: 400px;
+        }
+
+        .imagenPoke2 {
+            width: 100px;
+            height: 60px;
+        }
+    </style>
+
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <h1>Detalle Pokemons</h1>
     <div class="row">
         <% foreach (dominioP.Pokemon poke in listaPokemon)
             { %>
-        <div class="col-6">
-            <div class="mb-3">
+        <div class="col-lg-6 col-md-6 col-sm-12">
+            <div class="mb-3 custom-card">
                 <div class="card">
 
                     <img src="<%: poke.UrlImagen %>" class="card-img-top imagenPoke" alt="...">
@@ -18,15 +62,17 @@
                 </div>
             </div>
         </div>
-        <div class="col-6">
-            <div class="mb-3">
+        <div class="col-lg-6 col-md-6 col-sm-12">
+            <div class="mb-3 custom-card">
                 <div class="card">
-                    <div class="card-body">
-                        <p class="card-text">Número: <%: poke.Numero %> </p>
-                        <p class="card-text">Descripción: <%: poke.Descripcion %></p>
-                        <p class="card-text">Tipo: <%: poke.Tipo.Descripcion %></p>
-                        <p class="card-text">Debilidad: <%: poke.Tipo.Descripcion %></p>
-                        <a href="Favoritos.aspx?id=<%: poke.Id %>" style="text-decoration: none; font-size: 25px; color: darkcyan" title="Agregar a Favoritos!">★ </a>
+                    <div class="card-body imagenPoke">
+                        <p class="card-text"><strong>Número:</strong> <%: poke.Numero %> </p>
+                        <p class="card-text"><strong>Descripcion:</strong> <%: poke.Descripcion %></p>
+                        <p class="card-text"><strong>Tipo:</strong> <%: poke.Tipo.Descripcion %></p>
+                        <p class="card-text"><strong>Debilidad:</strong> <%: poke.Debilidad.Descripcion %></p>
+                        <a href="Favoritos.aspx?id=<%: poke.Id %>" class="btn-favorite"
+                            title="Agregar a Favoritos!">★ </a>
+                        <img src="<%: ObtenerRutaImagenTipo(poke.Tipo.Descripcion) %>" class="imagenPoke2" alt="<%: poke.Tipo.Descripcion %>">
                     </div>
                 </div>
             </div>
--- 5.ConexionesDB/pokedex-web/pokedex-web/DetallePokemon.aspx.cs
+++ 5.ConexionesDB/pokedex-web/pokedex-web/DetallePokemon.aspx.cs
@@ -14,20 +14,25 @@ namespace pokedex_web
         public List<Pokemon> listaPokemon { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
-            string id = Request.QueryString["id"] != null ? Request.QueryString["id"].ToString() : "";
-            if (id != "" && !IsPostBack)
+
+            try
             {
-                PokemonNegocio negocio = new PokemonNegocio();
-                listaPokemon = negocio.listar(id);
-                Pokemon seleccionado = listaPokemon[0];
-
-                /// Cargamos los datos del pokemon en los controles
-                
-
+                string id = Request.QueryString["id"] != null ? Request.QueryString["id"].ToString() : "";
+                if (!string.IsNullOrEmpty(id) || !IsPostBack)
+                {
+                    PokemonNegocio negocio = new PokemonNegocio();
+                    listaPokemon = negocio.listar(id);
+                }
+                else
+                {
+                    Response.Redirect("Default.aspx", false);
+                }
             }
-
-
-
+            catch (Exception ex)
+            {
+                Session.Add("error", ex.Message);
+                Response.Redirect("Error.aspx");
+            }
 
 
         }
@@ -35,6 +40,42 @@ namespace pokedex_web
         protected void btnVolver_Click(object sender, EventArgs e)
         {
             Response.Redirect("Default.aspx", false);
+        }
+
+        // Diccionario para mapear tipos de Pokémon a rutas de imágenes
+        private Dictionary<string, string> rutasImagenesPorTipo = new Dictionary<string, string>
+        {
+            { "Fuego", "https://upload.wikimedia.org/wikipedia/commons/a/a6/Fuego_Pokemon.svg" },
+            { "Planta", "https://upload.wikimedia.org/wikipedia/commons/f/f2/Planta_Pokemon.svg" },
+            { "Agua", "https://upload.wikimedia.org/wikipedia/commons/3/36/Agua_Pokemon.svg" },
+            { "Veneno", "https://upload.wikimedia.org/wikipedia/commons/e/e7/Veneno_Pokemon.svg" },
+            { "Psíquico", "https://upload.wikimedia.org/wikipedia/commons/b/b7/Ps%C3%ADquico_Pokemon.svg" },
+            { "Normal", "https://upload.wikimedia.org/wikipedia/commons/9/9d/Normal_Pokemon.svg" },
+            { "Bicho", "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Bicho_Pokemon.svg/2560px-Bicho_Pokemon.svg.png" },
+            { "Acero", "https://upload.wikimedia.org/wikipedia/commons/2/2e/Acero_Pokemon.svg" },
+            { "Eléctrico", "https://upload.wikimedia.org/wikipedia/commons/2/2b/El%C3%A9ctrico_Pokemon.svg" },
+            { "Dragón", "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Pok%C3%A9mon_type_Dragon_fr.svg/2560px-Pok%C3%A9mon_type_Dragon_fr.svg.png" },                       
+            { "Fantasma", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Fantasma_Pokemon.svg/2560px-Fantasma_Pokemon.svg.png" },
+            { "Hielo", "https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Hielo_Pokemon.svg/1280px-Hielo_Pokemon.svg.png" },
+            { "Lucha", "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Lucha_Pokemon.svg/1280px-Lucha_Pokemon.svg.png" },
+            { "Roca", "https://upload.wikimedia.org/wikipedia/commons/4/46/Roca_Pokemon.svg" },
+            { "Tierra", "https://upload.wikimedia.org/wikipedia/commons/4/43/Tierra_Pokemon.svg" },
+            { "Volador", "https://upload.wikimedia.org/wikipedia/commons/9/9d/Volador_Pokemon.svg" },
+        };
+
+        // Obtiene la ruta de la imagen del tipo
+        protected string ObtenerRutaImagenTipo(string tipo)
+        {
+            // Verificar si el tipo existe en el diccionario
+            if (rutasImagenesPorTipo.TryGetValue(tipo, out string rutaImagen))
+            {
+                return rutaImagen;
+            }
+            else
+            {
+                // Si no hay una imagen definida para el tipo devuelve una imagen por defecto
+                return "https://i.pinimg.com/originals/a7/73/64/a773645c4ef8e7c77eee0f70e3e9e5a4.jpg";
+            }
         }
     }
 }
